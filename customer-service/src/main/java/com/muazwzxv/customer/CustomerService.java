775015d261b947cb3d3591f50c5e324ea094c178
muazwzxv <muazwazir@gmail.com>
2022-02-06 03:01:53 +0800

    customer service

---
+++ customer-service/src/main/java/com/muazwzxv/customer/CustomerService.java
@@ -0,0 +1,11 @@
+package com.muazwzxv.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerService {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerService.class, args);
+    }
+}
