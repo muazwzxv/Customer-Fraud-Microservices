8ac52a7bb7badc40b93131cdfbbd294288d39e26
muazwzxv <muazwazir@gmail.com>
2022-02-09 02:29:30 +0800

    added rabbitmq support

--- notification-service/src/main/java/com/muazwzxv/notifications/NotificationController.java
+++ notification-service/src/main/java/com/muazwzxv/notifications/NotificationController.java
@@ -1,6 +1,7 @@
 package com.muazwzxv.notifications;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("/api/v1/notification")
 public class NotificationController {
 
@@ -15,6 +17,7 @@ public class NotificationController {
 
     @PostMapping
     public void send(@RequestBody NotificationRequestDto req) {
+        log.info("New Notification {}", req);
         notificationService.sendMessage(req);
     }
 }
